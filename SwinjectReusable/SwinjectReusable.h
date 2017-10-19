8a7acf50a985d03e3ccc27c35295369f96ae615c
Xavier De Koninck <xavier.dekoninck@gmail.com>
2017-10-19 15:21:46 +0200

    First version

---
+++ SwinjectReusable/SwinjectReusable.h
@@ -0,0 +1,19 @@
+//
+//  SwinjectReusable.h
+//  SwinjectReusable
+//
+//  Created by Xavier De Koninck on 19/10/2017.
+//  Copyright © 2017 Xavier De Koninck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwinjectReusable.
+FOUNDATION_EXPORT double SwinjectReusableVersionNumber;
+
+//! Project version string for SwinjectReusable.
+FOUNDATION_EXPORT const unsigned char SwinjectReusableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwinjectReusable/PublicHeader.h>
+
+
